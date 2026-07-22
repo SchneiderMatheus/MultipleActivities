002e31b65e44555413cf7954aafefc642efa0120
Matheus <matheuswschneider@gmail.com>
2026-07-22 17:43:26 -0300

    Base

---
+++ Atividade Contatos/Program/src/application/App.java
@@ -0,0 +1,15 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Contacts;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        List<Contacts> listaContatos = new ArrayList<>();
+        
+    }
+}
