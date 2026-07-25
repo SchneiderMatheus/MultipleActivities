8060d93ccc57ea01e5ea045c9c98bba8b6d82213
Matheus <matheuswschneider@gmail.com>
2026-07-24 22:56:29 -0300

    Contatos

--- Atividade Contatos/Program/src/application/App.java
+++ Atividade Contatos/Program/src/application/App.java
@@ -2,14 +2,42 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import entities.Contacts;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
 
         List<Contacts> listaContatos = new ArrayList<>();
-        
+        System.out.println("1 - Adiconar novo contato.");
+        System.out.println("2 - Visualizar contatos.");
+        System.out.println("3 - Atualizar o telefone e e-mail de um contato existente.");
+        System.out.println("4 - Remover um contato da lista.");
+        System.out.println("5 - Sair do programa.");
+        System.out.println("Selecione a opção desejada (1 até 5)");
+        int n = sc.nextInt();
+
+        switch (n) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            case 4:
+                
+                break;
+            case 5:
+                
+                break;
+            default:
+
+                return;
+        }
     }
 }
