cde734c37948e2af6bf8ff02869d6ceed5665611
Matheus <matheuswschneider@gmail.com>
2026-07-25 00:15:45 -0300

    Finished activity

--- Atividade Contatos/Program/src/application/App.java
+++ Atividade Contatos/Program/src/application/App.java
@@ -11,61 +11,72 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         List<Contacts> listaContatos = new ArrayList<>();
+        int opcao;
+        do {
         System.out.println("1 - Adicionar novo contato.");
         System.out.println("2 - Visualizar contatos.");
         System.out.println("3 - Atualizar o telefone e e-mail de um contato existente.");
         System.out.println("4 - Remover um contato da lista.");
         System.out.println("5 - Sair do programa.");
         System.out.println("Selecione a opção desejada (1 até 5)");
-        int n = sc.nextInt();
+        opcao = sc.nextInt();
         sc.nextLine();
 
-        switch (n) {
-            case 1:
-                System.out.println("Digite as informações do novo contato: ");
-                System.out.print("Nome: ");
-                String name = sc.nextLine();
-                System.out.print("telefone: ");
-                String phone = sc.nextLine();
-                System.out.print("E-mail: ");
-                String email = sc.nextLine();
-                Contacts contato = new Contacts(name,phone,email);
-                listaContatos.add(contato);
-                System.out.println();
-                break;
-            case 2:
-                System.out.println("Lista de Contatos: ");
-                for (Contacts contacts : listaContatos) {
-                System.out.println(contacts);
-                }
-                break;
-            case 3:
-                System.out.println("Informe o contato que você gostaria de mudar as informações");
-                String nameVerificar = sc.nextLine();
-                for (Contacts contatos : listaContatos){
-                    if (nameVerificar.equalsIgnoreCase(contatos.getName())) {
-                        System.out.println("Digite o novo telefone: ");
-                        String phoneNovo = sc.nextLine();
-                        contatos.setPhone(phoneNovo);
-                        System.out.println("Digite o novo e-mail: ");
-                        String emailNovo = sc.nextLine();
-                        contatos.setEmail(emailNovo);
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite as informações do novo contato: ");
+                    System.out.print("Nome: ");
+                    String name = sc.nextLine();
+                    System.out.print("telefone: ");
+                    String phone = sc.nextLine();
+                    System.out.print("E-mail: ");
+                    String email = sc.nextLine();
+                    Contacts contato = new Contacts(name,phone,email);
+                    listaContatos.add(contato);
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println("\nLista de Contatos: ");
+                    for (Contacts contacts : listaContatos) {
+                    System.out.println(contacts);
+                    System.out.println();
                     }
-                    else 
-                        System.out.println("Contato não encontrado!");
-                }
+                    break;
+                case 3:
+                    System.out.println("Informe o contato que você gostaria de mudar as informações");
+                    String nameVerificar = sc.nextLine();
+                    for (Contacts contatos : listaContatos){
+                        if (nameVerificar.equalsIgnoreCase(contatos.getName())) {
+                            System.out.println("Digite o novo telefone: ");
+                            String phoneNovo = sc.nextLine();
+                            contatos.setPhone(phoneNovo);
+                            System.out.println("Digite o novo e-mail: ");
+                            String emailNovo = sc.nextLine();
+                            contatos.setEmail(emailNovo);
+                        }
+                        else 
+                            System.out.println("Contato não encontrado!");
+                    }
 
-                break;
-            case 4:
-                
-                break;
-            case 5:
-                
-                break;
-            default:
+                    break;
+                case 4:
+                    System.out.println("Informe o contato a ser removido: ");
+                    String nameRemove = sc.nextLine();
+                    for (Contacts contatos : listaContatos) {
+                        if (nameRemove.equalsIgnoreCase(contatos.getName())) {
+                            listaContatos.remove(contatos);
+                        }
+                    }
+                    break;
+                case 5:
+                    System.out.println("Finalizando o Programa...!");
+                    opcao =5;
+                    break;
+                default:
 
-                break;
-        }
+                    break;
+            }
+        } while (opcao != 5);
         sc.close();
         return;
     }
