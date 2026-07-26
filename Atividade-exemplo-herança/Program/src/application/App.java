108d39e49ecf7db0062182d22a94532b12b72de9
Matheus <matheuswschneider@gmail.com>
2026-07-26 18:33:54 -0300

    Finished

--- Atividade-exemplo-herança/Program/src/application/App.java
+++ Atividade-exemplo-herança/Program/src/application/App.java
@@ -1,7 +1,18 @@
 package application;
+
+import java.util.Scanner;
+
+import entities.BusinessAccount;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
+        Scanner sc = new Scanner(System.in);
+        BusinessAccount contaEmpresa = new BusinessAccount(1,"Matheus",9.5,1000.50);
+
+        System.out.println("Valor do emprestimo:");
+        contaEmpresa.loan(sc.nextDouble());
+
+        System.err.println(contaEmpresa.getBalance());
+        sc.close();
     }
 }
