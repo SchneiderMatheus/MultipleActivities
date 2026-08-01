d7b1ff58e7483e29ec08624d953b29cd4a0ebf01
Matheus <matheuswschneider@gmail.com>
2026-07-31 23:13:09 -0300

    Upcasting

--- Atividade-exemplo-herança/Program/src/application/App.java
+++ Atividade-exemplo-herança/Program/src/application/App.java
@@ -2,17 +2,24 @@ package application;
 
 import java.util.Scanner;
 
+import entities.Account;
 import entities.BusinessAccount;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        Account acc = new Account(2, "Alex", 0.0);
         BusinessAccount contaEmpresa = new BusinessAccount(1,"Matheus",9.5,1000.50);
 
         System.out.println("Valor do emprestimo:");
         contaEmpresa.loan(sc.nextDouble());
 
         System.err.println(contaEmpresa.getBalance());
+
+        // upcasting
+
+        Account acc1 = contaEmpresa;
+        Account acc2 = new BusinessAccount(3, "Anna", 0.0, 1000.0);
         sc.close();
     }
 }
