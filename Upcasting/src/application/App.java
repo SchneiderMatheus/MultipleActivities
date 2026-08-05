1aca2c1671e015942674df6ae582bb52ffc94f08
Matheus <matheuswschneider@gmail.com>
2026-08-04 22:21:25 -0300

    Exemplo simples

--- Upcasting/src/application/App.java
+++ Upcasting/src/application/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entre com a sua nota da m1: ");
+        double m1 = sc.nextDouble();
+        System.out.println("Entre com a sua nota da m2: ");
+        double m2 = sc.nextDouble();
+        System.out.println("Entre com a sua nota da m3: ");
+        double m3 = sc.nextDouble();
+
+        double media = (m1 + m2 + m3) / 3;
+        System.out.println("Sua média é: " + media);
+        
+        sc.close();
     }
 }
