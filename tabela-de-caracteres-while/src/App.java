cb8a67463b892bc31438cf351ff73a1bc75e80b7
Matheus <matheuswschneider@gmail.com>
2026-08-05 22:25:59 -0300

    Atividade

--- tabela-de-caracteres-while/src/App.java
+++ tabela-de-caracteres-while/src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        char contador='a';
+        int valorNumerico = 0;
+        while (valorNumerico <= 121) {
+            System.out.println("Caractere = "+contador);
+            valorNumerico = contador;
+            System.out.println("Valor correspondente = "+valorNumerico);
+            contador ++;
+            System.out.println("------------------------------------");
+        }
     }
 }
