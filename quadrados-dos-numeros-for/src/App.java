344a7c3c0083fca007f40744d25da44bb7072b8b
Matheus <matheuswschneider@gmail.com>
2026-08-06 17:45:21 -0300

    Atividades

--- quadrados-dos-numeros-for/src/App.java
+++ quadrados-dos-numeros-for/src/App.java
@@ -1,6 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
+        for (int i = 0; i <= 100; i++) {
+            System.out.println("Valor atual: "+i);
+            System.out.println("Valor ao quadrado: "+Math.pow(i, 2));
+            System.out.println("------------------------------------------");
+        }
 
     }
 }
