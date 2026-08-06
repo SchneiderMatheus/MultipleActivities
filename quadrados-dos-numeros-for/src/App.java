c4e5a8e99679caa14f098ab84920f114e690de0d
Matheus <matheuswschneider@gmail.com>
2026-08-06 17:46:33 -0300

    Fix

--- quadrados-dos-numeros-for/src/App.java
+++ quadrados-dos-numeros-for/src/App.java
@@ -4,7 +4,7 @@ public class App {
         for (int i = 0; i <= 100; i++) {
             System.out.println("Valor atual: "+i);
             System.out.println("Valor ao quadrado: "+Math.pow(i, 2));
-            System.out.println("------------------------------------------");
+            System.out.println("----------------------------------------------------------------------------------");
         }
 
     }
