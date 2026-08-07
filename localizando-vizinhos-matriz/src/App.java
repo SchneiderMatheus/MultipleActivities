c73e6e78908b7d098e44f98a06a7e23151514deb
Matheus <matheuswschneider@gmail.com>
2026-08-07 15:40:12 -0300

    Atividade

--- localizando-vizinhos-matriz/src/App.java
+++ localizando-vizinhos-matriz/src/App.java
@@ -5,22 +5,20 @@ public class App {
     public static void main(String[] args) throws Exception {
         Random random = new Random();
         Scanner sc = new Scanner(System.in);
-        
-        int [][] matriz = new int[3][3];
+
+        int[][] matriz = new int[3][3];
         for (int i = 0; i < matriz.length; i++) {
-            int linha,coluna;
+            
             for (int j = 0; j < matriz.length; j++) {
-                 do {
-                linha = random.nextInt(2);
-                coluna = random.nextInt(2);
-            } while (matriz[linha][coluna]!= '0');
-            matriz[linha][coluna] = random.nextInt(9);
+                
+                matriz[i][j] = random.nextInt(10);
             }
         }
-        
+
         for (int i = 0; i < matriz.length; i++) {
+
             for (int j = 0; j < matriz.length; j++) {
-                System.out.println(" "+matriz[i][j]);
+                System.out.print(" " + matriz[i][j]);
             }
             System.out.println();
         }
