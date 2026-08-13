0e8c035bd487f5c040d23c4f13a0a85a87991444
Matheus <matheuswschneider@gmail.com>
2026-08-13 20:37:09 -0300

    Grãos

--- graos-arroz/src/App.java
+++ graos-arroz/src/App.java
@@ -10,10 +10,15 @@ public class App {
         java.math.BigInteger totalGraos = java.math.BigInteger.ZERO;
         java.math.BigInteger graosCasa = java.math.BigInteger.ONE;
 
+        int casa100, casa1000;
+
         for (int casa=1; casa <= 64; casa++) {
             totalGraos = totalGraos.add(graosCasa);
-            System.out.println("Casa: "+casa+", Grãos: "+graosCasa);
+            System.out.println("Casa: "+casa+", Grão na casa: "+graosCasa+" Total Acumulado: "+totalGraos);
             graosCasa = graosCasa.multiply(java.math.BigInteger.valueOf(2));
+            if (totalGraos.compareTo(totalGraos) >= 100) {
+                
+            }
         }
         
         System.out.println("Grãos: "+totalGraos);
