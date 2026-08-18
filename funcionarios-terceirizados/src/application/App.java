2b0c65d3065750a2b9947e4bfde40df37d7afaa5
Matheus <matheuswschneider@gmail.com>
2026-08-17 22:34:39 -0300

    Applications

--- funcionarios-terceirizados/src/application/App.java
+++ funcionarios-terceirizados/src/application/App.java
@@ -1,13 +1,22 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+
+import entities.Employee;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        List <Employee> list = new ArrayList<>();
+        
+        System.out.println("Enter the number of emplyees: ");
+        int n = sc.nextInt();
+        
         sc.close();
     }
 }
