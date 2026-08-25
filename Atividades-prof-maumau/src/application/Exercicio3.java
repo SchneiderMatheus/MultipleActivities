ad2c45ed1b5634d7724d4a019367f37bc876ca46
Matheus <matheuswschneider@gmail.com>
2026-08-25 16:02:12 -0300

    Funções

---
+++ Atividades-prof-maumau/src/application/Exercicio3.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        System.out.println("MENU");
+        exibirMenu();
+        
+    }
+
+    public static void exibirMenu(){
+        System.out.println("1 - Somar \n2 - Subtrair \n3 - Multiplicar \n4 - Dividir");
+    }
+}
