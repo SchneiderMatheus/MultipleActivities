14ab4ef909ac9824d8bdbb52581c8fb5e31fe012
Matheus <matheuswschneider@gmail.com>
2026-08-28 16:07:17 -0300

    Templates

---
+++ Atividades-prof-maumau/src/application/Templates.java
@@ -0,0 +1,18 @@
+package application;
+
+public class Templates {
+    public static void main(String[] args) {
+        /*
+        Um template permite criar funções ou estruturas genéricas, capazes de
+        trabalhar com diferentes tipos de dados sem precisar escrever uma
+        implementação específica para cada tipo.
+        Objetivos:
+        ❑ Evitar duplicação de código.
+        ❑ Criar funções que possam trabalhar com diferentes tipos de dados.
+        ❑ Reutilizar a mesma lógica para int, double, float, entre outros tipos.
+        ❑ Permitir que o compilador determine o tipo adequado a partir dos
+        argumentos utilizados.
+        ❑ Facilitar a criação de funções genéricas para vetores e matrizes.
+         */
+    }
+}
