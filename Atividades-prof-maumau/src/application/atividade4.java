c1b783b3b273a3613993780d541a3beee7239dee
Matheus <matheuswschneider@gmail.com>
2026-09-03 19:53:21 -0300

    Try

--- Atividades-prof-maumau/src/application/atividade4.java
+++ Atividades-prof-maumau/src/application/atividade4.java
@@ -3,12 +3,16 @@ package application;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Produto;
+
 public class atividade4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        
+        Produto produto = new Produto(98777, "Celular Iphone", 7000.9, 3);
+        System.out.println(produto);
         sc.close();
     }
+
 }
