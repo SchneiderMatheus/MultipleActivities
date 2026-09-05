04ac542af6903adea2f6b9ff02ce908ec606a4c0
Matheus <matheuswschneider@gmail.com>
2026-09-05 17:46:52 -0300

    try

--- Atividades-prof-maumau/src/application/Atividade5.java
+++ Atividades-prof-maumau/src/application/Atividade5.java
@@ -1,5 +1,25 @@
 package application;
 
-public class Atividade5 {
+import java.util.Scanner;
 
+import entities.ProdutoAtividade5;
+
+public class Atividade5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ProdutoAtividade5[] produto = new ProdutoAtividade5[5];
+
+        for (int i = 0; i < produto.length; i++) {
+            System.out.println("Adicione o nome do produto "+(i+1));
+            String nome = sc.nextLine();
+            System.out.println("Adicione o codigo do produto: "+(i+1));
+            Integer codigo = sc.nextInt();
+            System.out.println("Adicione o preço do produto: "+(i+1));
+            Double preco = sc.nextDouble();
+
+            produto[i].setNome(nome);
+            produto[i].setCodigo(codigo);
+            produto[i].setPreco(preco);
+        }
+    }
 }
--- Atividades-prof-maumau/src/entities/ProdutoAtividade5.java
+++ Atividades-prof-maumau/src/entities/ProdutoAtividade5.java
@@ -11,5 +11,29 @@ public class ProdutoAtividade5 {
         this.preco = preco;
     }
 
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
     
 }
