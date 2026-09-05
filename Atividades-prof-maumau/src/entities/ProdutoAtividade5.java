4a10f6d33fb251d61d22068d7af0fb280f6c4d6a
Matheus <matheuswschneider@gmail.com>
2026-09-05 17:33:13 -0300

    Constructors

--- Atividades-prof-maumau/src/entities/ProdutoAtividade5.java
+++ Atividades-prof-maumau/src/entities/ProdutoAtividade5.java
@@ -1,5 +1,15 @@
 package entities;
 
 public class ProdutoAtividade5 {
+    private Integer codigo;
+    private String nome;
+    private Double preco;
+    
+    public ProdutoAtividade5(Integer codigo, String nome, Double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
     
 }
