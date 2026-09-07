9fa521b017f380fd9d9710ae68766ba5f54c3d64
Matheus <matheuswschneider@gmail.com>
2026-09-07 19:42:55 -0300

    ...

--- StudentGradeManagement/Program/src/entities/Teacher.java
+++ StudentGradeManagement/Program/src/entities/Teacher.java
@@ -39,4 +39,7 @@ public class Teacher extends Person {
     public void giveRaise(Double raise){
         this.salary = salary * (raise/100);
     }
+    public String displayInfo(){
+        return "Name: " + getName()+", Age: "+getAge()+", Teacher ID: "+teacherId+", Subject: "+subject+", Salary: "+salary;
+    }
 }
