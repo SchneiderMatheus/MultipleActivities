91cc00b432a57012c3aab3740d387cfbaf0e8dc4
Matheus <matheuswschneider@gmail.com>
2026-09-07 19:39:55 -0300

    Teacher

--- StudentGradeManagement/Program/src/applications/App.java
+++ StudentGradeManagement/Program/src/applications/App.java
@@ -9,6 +9,7 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         
+        
         sc.close();
     }
 }
---
+++ StudentGradeManagement/Program/src/entities/Teacher.java
@@ -0,0 +1,42 @@
+package entities;
+
+public class Teacher extends Person {
+    private String teacherId;
+    private String subject;
+    private Double salary;
+    
+    public Teacher(String name, Integer age, String teacherId, String subject, Double salary) {
+        super(name, age);
+        this.teacherId = teacherId;
+        this.subject = subject;
+        this.salary = salary;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    
+    public void giveRaise(Double raise){
+        this.salary = salary * (raise/100);
+    }
+}
