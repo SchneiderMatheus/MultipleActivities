d1a93b002527ab7a4af4729f90d60a30149293de
Matheus <matheuswschneider@gmail.com>
2026-09-10 19:02:46 -0300

    ...

--- StudentGradeManagement/Program/src/applications/App.java
+++ StudentGradeManagement/Program/src/applications/App.java
@@ -13,15 +13,17 @@ public class App {
 
         Teacher teacher = new Teacher("Matheus", 29, "123456", "History", 4590.5);
         System.out.println(teacher);
-
+        System.out.println("-----------------------------------------------------------------------");
         System.out.println("Quanto de aumento o professor deve receber?");
         double n = sc.nextDouble();
         teacher.giveRaise(n);
         System.out.println(teacher);
+        System.out.println("-----------------------------------------------------------------------");
         
         double[] numeros = {9.0,7.5,8,7.5,3};
         Student estudante = new Student("Matheus", 29, "123456", numeros);
         System.out.println(estudante);
+        System.out.println("-----------------------------------------------------------------------");
         
         sc.close();
     }
