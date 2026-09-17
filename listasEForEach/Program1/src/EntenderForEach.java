ff82e96c0200a6ea1c047ff1b502a41e9aebdf7f
Matheus <matheuswschneider@gmail.com>
2026-09-17 19:21:28 -0300

    1

--- listasEForEach/Program1/src/EntenderForEach.java
+++ listasEForEach/Program1/src/EntenderForEach.java
@@ -1,5 +1,15 @@
 public class EntenderForEach {
     public static void main(String[] args) {
-        
+        String[] fruits = {"apple", "banana", "cherry"};
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+        System.out.println("_________________________________________________________________________\n");
+
+        for (String i : fruits) {
+            System.out.println(i);
+        }
     }
 }
