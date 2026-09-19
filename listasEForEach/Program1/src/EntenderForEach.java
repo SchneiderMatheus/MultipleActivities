fb60fe4713787ed6c84b4bfab0c26ede5c3a9aee
Matheus <matheuswschneider@gmail.com>
2026-09-19 15:38:57 -0300

    Done

--- listasEForEach/Program1/src/EntenderForEach.java
+++ listasEForEach/Program1/src/EntenderForEach.java
@@ -50,10 +50,8 @@ public class EntenderForEach {
         int[] inteiros5 ={9, 4, 15, 2, 7};
         int small=100;
         for (int i : inteiros5) {
-            if (i>0) {
-                if (i<(inteiros5[i-1])) {
-            small = inteiros5[i];
-                }
+            if (i < small) {
+                small = i;
             }
             
         }
